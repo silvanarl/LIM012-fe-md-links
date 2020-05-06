3db773153cb7f9c2a024bbece85e4f674036f268
silvanarl <ramossilvana495@gmail.com>
2020-05-06 09:39:57 -0500

    test extractLink ok

--- test/main.spec.js
+++ test/main.spec.js
@@ -3,20 +3,66 @@ const {
   isAbsolutePath,
   converterRelativeToAbsolutePath,
   isDirectoryPath,
-  isFileMdPath,
+  routeExtension,
   readDirectoryPath,
-  readFilePath
+  readFilePath,
+  extractLinks
 } = require('../src/main.js');
 
-const output1 = ['file1.txt', 'file2.md', 'file3.html', 'otherFiles'];
-
-// const output2 = 'it says here anything', 
-// 'google https://www.google.com/ ',
-// 'slack https://slack.com/intl/es-pe/',
-// 'stackoverflow https://es.stackoverflow.com/';
-
-const output3 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-
+const output1 = [
+'test\\example\\evenMoreExamples\\file1.txt', 
+'test\\example\\evenMoreExamples\\file2.md', 
+'test\\example\\evenMoreExamples\\file3.html', 
+'test\\example\\evenMoreExamples\\otherFiles'];
+const output2 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const input1 = ['test\\example\\evenMoreExamples\\file2.md']
+const output3 = [
+  { 
+    href: 'https://nodejs.org/en/',
+    title: null, 
+    text: 'Node.js' 
+  },
+  {
+    href: 'https://nodejs.org/docs/latest-v0.10.x/api/modules.html',
+    title: null,
+    text: 'módulos (CommonJS)'
+  },
+  {
+    href: 'https://nodejs.org/api/fs.html',
+    title: null,
+    text: 'file system'
+  },
+  {
+    href: 'https://nodejs.org/api/path.html',
+    title: null,
+    text: 'path'
+  },
+  {
+    href: 'https://nodejs.org/api/http.html#http_http_get_options_callback',
+    title: null,
+    text: 'http.get'
+  },
+  {
+    href: 'https://daringfireball.net/projects/markdown/syntax',
+    title: null,
+    text: 'markdown'
+  },
+  {
+    href: 'https://docs.npmjs.com/misc/scripts',
+    title: null, 
+    text: 'npm-scripts' 
+  },
+  { 
+    href: 'https://semver.org/',
+    title: null, 
+    text: 'semver' 
+  },
+  { 
+    href: 'https://www.google.com',
+    title: 'Google&#39;s Homepage',
+    text: 'I&#39;m an inline-style link with title'
+  }
+];
 describe('isValidPath', () => {
     it('should be a function', () => {
       expect(typeof isValidPath).toBe('function');
@@ -65,16 +111,16 @@ describe('isDirectoryPath', () => {
   });
 });
 
-describe('isFileMdPath', () => {
+describe('routeExtension', () => {
   it('should be a function', () => {
-    expect(typeof isFileMdPath).toBe('function');
+    expect(typeof routeExtension).toBe('function');
   });
   it('should return the file extension', () => {
-      expect(isFileMdPath('./test/example/sample_text.md')).toBe('.md');
+      expect(routeExtension('./test/example/sample_text.md')).toBe('.md');
   });
 
   it('should return the file extension', () => {
-      expect(isFileMdPath('C:/Users/acer/Desktop/LIM012-fe-md-links/test/example/evenMoreExamples/file3.html')).toBe('.html');
+      expect(routeExtension('C:/Users/acer/Desktop/LIM012-fe-md-links/test/example/evenMoreExamples/file3.html')).toBe('.html');
   });
 });
 
@@ -92,6 +138,15 @@ describe('readFilePath', () => {
     expect(typeof readFilePath).toBe('function');
   });
   it('should return the contents of the file read', () => {
-      expect(readFilePath('./test/example/moreExamples/readme2.md')).toEqual(output3);
+      expect(readFilePath('./test/example/moreExamples/readme2.md')).toEqual(output2);
+  });
+});
+
+describe('extractLinks', () => {
+  it('should be a function', () => {
+    expect(typeof extractLinks).toBe('function');
+  });
+  it('should return an array of objects with the properties of each found link', () => {
+      expect(extractLinks(input1)).toEqual(output3);
   });
 });
