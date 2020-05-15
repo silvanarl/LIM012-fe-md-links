6840907719929e7e054e629beb97a04d4dc59e28
silvanarl <ramossilvana495@gmail.com>
2020-05-14 20:00:19 -0500

    mdLinks modificada

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,14 +1,31 @@
-const mainFunctions = require('./main.js');
+const {extractLinks, validateLinks} = require('./main.js');
+
+const help = `
+-----------------Valid Arguments-----------------
+md-links <path-to-file> --validate   --stats
+md-links <path-to-file> --v   --s
+md-links <path-to-file> --validate
+md-links <path-to-file> --stats
+-------------------------------------------------
+`;
 
 const mdLinks = (path, options) => {
     const links = new Promise((resolve) => {
-        resolve(mainFunctions.extractLinks(path));
+        if (options.validate === true) {
+            resolve(validateLinks(path)); 
+        }
+        if (path) {
+            resolve(extractLinks(path));
+        }
     });
-    if (options.validate === true) {
-        return mainFunctions.validateLinks(path);
-    }
-    return links;
+    return links;   
 };
 console.log(mdLinks('./test/example/sample_text.md', {validate: true}));
-
 module.exports = mdLinks;
+
+// const marckdownLinks = (path, options) => {
+//     return new Promise((resolve, reject) =>{
+//         const links;
+//         .then((resolve) => extractLinks(path))
+//     })
+// }
