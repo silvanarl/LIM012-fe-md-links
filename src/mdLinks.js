a922aa9f41798f66b1caf737e28d9ced83450d78
silvanarl <ramossilvana495@gmail.com>
2020-05-16 23:37:04 -0500

    mdLinks mdoficado

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,14 +1,5 @@
 const {extractLinks, validateLinks} = require('./main.js');
 
-const help = `
------------------Valid Arguments-----------------
-md-links <path-to-file> --validate   --stats
-md-links <path-to-file> --v   --s
-md-links <path-to-file> --validate
-md-links <path-to-file> --stats
--------------------------------------------------
-`;
-
 const mdLinks = (path, options) => {
     const links = new Promise((resolve) => {
         if (options.validate === true) {
@@ -20,12 +11,4 @@ const mdLinks = (path, options) => {
     });
     return links;   
 };
-console.log(mdLinks('./test/example/sample_text.md', {validate: true}));
 module.exports = mdLinks;
-
-// const marckdownLinks = (path, options) => {
-//     return new Promise((resolve, reject) =>{
-//         const links;
-//         .then((resolve) => extractLinks(path))
-//     })
-// }
