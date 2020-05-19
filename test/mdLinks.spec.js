1fdcdb331d86f3e9181aff80315b3104fcff37e4
silvanarl <ramossilvana495@gmail.com>
2020-05-18 20:44:37 -0500

    test al 100%

--- test/mdLinks.spec.js
+++ test/mdLinks.spec.js
@@ -9,16 +9,15 @@ describe('mdLinks', () => {
     it('should be a function', () => {
       expect(typeof mdLinks).toBe('function');
     });
-    test('returns an array with five properties for each link', (done) => {
+    it('returns an array with five properties for each link', () => {
+        expect.assertions(1);
         return mdLinks('./test/example/sample_text.md', {validate: true}).then((response) => {
             expect(response).toEqual(validateLinksOutput)
         });
-        done();
     });
-    test('returns an array with three properties for each link', (done) => {
+    it('returns an array with three properties for each link', () => {
         return mdLinks('./test/example/moreExamples/readme1.md', {validate: false}).then((response) => {
             expect(response).toEqual(extractLinksOutput)
         });
-        done();
     });
   });
